ce5b6906b5c4acdfa8c3dc93d53c19245c430eac
huangyugui <huangyugui@gomeholdings.com>
2017-04-16 23:14:39 +0800

    删除多余的代码

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-/////devdev
-/////master
-//master
-//
 public class DemoApplication {
     
 
